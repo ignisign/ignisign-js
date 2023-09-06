c69423acd65e42322067969a9c13dfd3f5864868
Loic-Fritsch <loic.fritsch.fr.com>
2023-09-06 13:51:00 +0200

    remove-examples

--- examples/npm-based/src/utils/front-url-provider.ts
+++
@@ -1,7 +0,0 @@
-export const  FrontUrlProvider = {
-  homePage                       : () => "/",
-  usersPage                      : () => "/users",
-  signatureRequestsPage          : () => '/signature-requests',
-  signatureRequestCreationPage   : () => '/signature-request-creation',
-  signatureRequestsDetailPage    : (signatureRequestId = null) => `/signature-request/${signatureRequestId || ':signatureRequestId'}`,
-}
